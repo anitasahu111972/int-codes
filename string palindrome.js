87aa790fde36936256d2a43c4bd08240937b2d3e
pankuuuuu <50828185+pankuuuuu@users.noreply.github.com>
2021-10-01 09:30:29 +0530

    herictal and derictal added

--- string palindrome.js
+++ string palindrome.js
@@ -1,12 +1,6 @@
 //check palindrome
 
 function palindrome(str) {
- // Step 1. The first part is the same as earlier
- var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
- str = str.toLowerCase().replace(re, '');
-
- // Step 2. Create the FOR loop
- var len = str.length; // var len = "A man, a plan, a canal. Panama".length = 30
  
  for (var i = 0; i < len/2; i++) {
    if (str[i] !== str[len - 1 - i]) { // As long as the characters from each part match, the FOR loop will go on
@@ -32,7 +26,32 @@ function palindrome(str) {
      16th iteration:       15        no               
     End of the FOR Loop*/
  }
+ 
+ // Step 1. The first part is the same as earlier
+ var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
+ str = str.toLowerCase().replace(re, '');
+
+ // Step 2. Create the FOR loop
+ var len = str.length; // var len = "A man, a plan, a canal. Panama".length = 30
+ 
  return true; // Both parts are strictly equal, it returns true => The string is a palindrome
 }
+ 
+function herictal(){
+   // Step 1. The first part is the same as earlier
+ var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
+ str = str.toLowerCase().replace(re, '');
 
+ // Step 2. Create the FOR loop
+ var len = str.length; // var len = "A man, a plan, a canal. Panama".length = 30
+}
+
+function derictal(){
+   // Step 1. The first part is the same as earlier
+ var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
+ str = str.toLowerCase().replace(re, '');
+
+ // Step 2. Create the FOR loop
+ var len = str.length; // var len = "A man, a plan, a canal. Panama".length = 30
+}
 palindrome("A man, a plan, a canal. Panama");
